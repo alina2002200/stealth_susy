098beb0e2ed9f1ce5ab8566662d4af6befa70626
alina2002200 <71118359+alina2002200@users.noreply.github.com>
2026-02-08 19:43:33 +0300

    Update vertices.py

--- Stealth_SUSY/vertices.py
+++ Stealth_SUSY/vertices.py
@@ -23,7 +23,7 @@ V_2196 = Vertex(name = 'V_2196',
 
 V_SYY = Vertex(name = 'V_SYY',
               particles = [ P.S__tilde__, P.Y, P.Y__tilde__ ],
-              color = [ 'd(2,3,4)' ],
+              color = [ 'f(2,3,4)' ],
               lorentz = [ L.FFS1 ],
               couplings = {(0,0):C.GC_SYY})
 
