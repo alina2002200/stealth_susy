d60d622a1f1d0f2ce010b4077f8886b3db203c5c
alina2002200 <71118359+alina2002200@users.noreply.github.com>
2026-02-15 18:45:22 +0300

    Update __init__.py

--- Stealth_SUSY_NLO/__init__.py
+++ Stealth_SUSY_NLO/__init__.py
@@ -41,6 +41,12 @@ else:
    all_CTvertices = CT_vertices.all_CTvertices
 
 
+__has_loop_hybrid__ = True
+__has_loop_orig__ = True
+__has_loop_ufo__ = True
+__loop_model__ = True
+__nlo_model__ = True
+
 gauge = [0, 1]
 
 
