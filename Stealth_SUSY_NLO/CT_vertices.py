5518b3a578f59e2240c7b2812a3c9bb257a016df
alina2002200 <71118359+alina2002200@users.noreply.github.com>
2026-02-15 22:26:24 +0300

    Update CT_vertices.py

--- Stealth_SUSY_NLO/CT_vertices.py
+++ Stealth_SUSY_NLO/CT_vertices.py
@@ -14,16 +14,16 @@ V_Sgg_R2 = CTVertex(name = 'V_Sgg_R2',
                     color = [ 'Identity(1,2)' ],
                     lorentz = [ L.VVS1 ],
                     loop_particles = [ [ [P.Y] ] ],
-                    couplings = {(0,0,0):C.GC_Sgg_r2})
+                    couplings = {(0,0,0):C.R2GC_Sgg})
 
-# R2 вершина для S Y Y (нужна для полноты)
+# R2 вершина для S Y Y 
 V_SYY_R2 = CTVertex(name = 'V_SYY_R2',
                     type = 'R2',
                     particles = [ P.Y__tilde__, P.Y, P.S ],
                     color = [ 'Identity(1,2)' ],
                     lorentz = [ L.FFS2 ],
                     loop_particles = [ [ [P.g, P.Y] ] ],
-                    couplings = {(0,0,0):C.GC_SYY_r2})
+                    couplings = {(0,0,0):C.R2GC_SYY})
 
 # UV вершина для пропагатора Y
 V_YY_UV = CTVertex(name = 'V_YY_UV',
@@ -32,7 +32,7 @@ V_YY_UV = CTVertex(name = 'V_YY_UV',
                    color = [ 'Identity(1,2)' ],
                    lorentz = [ L.FF1 ],
                    loop_particles = [ [ [P.g, P.Y] ] ],
-                   couplings = {(0,0,0):C.GC_Sgg_ct})
+                   couplings = {(0,0,0):C.UVGC_Sgg})
 
 # UV вершина для S Y Y (контрчлен)
 V_SYY_UV = CTVertex(name = 'V_SYY_UV',
@@ -41,7 +41,7 @@ V_SYY_UV = CTVertex(name = 'V_SYY_UV',
                     color = [ 'Identity(1,2)' ],
                     lorentz = [ L.FFS2 ],
                     loop_particles = [ [ [P.g, P.Y] ] ],
-                    couplings = {(0,0,0):C.GC_SYY_ct})
+                    couplings = {(0,0,0):C.UVGC_SYY})
 
 # UV вершина для Y Y g (контрчлен)
 V_YYg_UV = CTVertex(name = 'V_YYg_UV',
@@ -50,7 +50,7 @@ V_YYg_UV = CTVertex(name = 'V_YYg_UV',
                     color = [ 'T(3,2,1)' ],
                     lorentz = [ L.FFV1 ],
                     loop_particles = [ [ [P.g, P.Y] ] ],
-                    couplings = {(0,0,0):C.GC_YgY})
+                    couplings = {(0,0,0):C.UVGC_YYg})
 
 # UV вершина для S -> gg (контрчлен)
 V_Sgg_UV = CTVertex(name = 'V_Sgg_UV',
@@ -59,7 +59,7 @@ V_Sgg_UV = CTVertex(name = 'V_Sgg_UV',
                     color = [ 'Identity(1,2)' ],
                     lorentz = [ L.VVS1 ],
                     loop_particles = [ [ [P.Y] ] ],
-                    couplings = {(0,0,0):C.GC_Sgg_ct})
+                    couplings = {(0,0,0):C.UVGC_Sgg})
 
 # Список всех CT-вершин для экспорта
 all_CTvertices = [
