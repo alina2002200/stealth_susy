6d5fb32a09f0ecc0a797a4b681e813778597393b
alina2002200 <71118359+alina2002200@users.noreply.github.com>
2026-02-15 16:56:58 +0300

    Update couplings.py

--- Stealth_SUSY_NLO/couplings.py
+++ Stealth_SUSY_NLO/couplings.py
@@ -1,7 +1,8 @@
 # This file was automatically created by FeynRules 1.7.221
 # Mathematica version: 9.0 for Mac OS X x86 (64-bit) (January 24, 2013)
 # Date: Mon 7 Oct 2013 12:36:22
-
+import math
+pi = math.pi
 
 from object_library import all_couplings, Coupling
 
