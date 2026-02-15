9ddc50750300b2f24b1855ef2d3f2a503632eaa7
alina2002200 <71118359+alina2002200@users.noreply.github.com>
2026-02-15 23:59:46 +0300

    Update CT_couplings.py

--- Stealth_SUSY_NLO/CT_couplings.py
+++ Stealth_SUSY_NLO/CT_couplings.py
@@ -7,24 +7,23 @@ from object_library import all_couplings, Coupling
 from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
 import cmath
 
-pi2 = cmath.pi**2
 
 R2GC_Sgg = Coupling(name = 'R2GC_Sgg',
-                    value = 'complex(0,1)*(-0.5)*gY*gY/(16*pi2)',
+                    value = 'complex(0,1)*(-0.5)*gY*gY/(16*9.86960440109)',
                     order = {'QCD': 2, 'QED': 0})
 
 R2GC_SYY = Coupling(name = 'R2GC_SYY',
-                    value = 'complex(0,1)*(-3)*gY/(16*pi2)',
+                    value = 'complex(0,1)*(-3)*gY/(16*9.86960440109)',
                     order = {'QCD': 1, 'QED': 1})
 
 UVGC_Sgg = Coupling(name = 'UVGC_Sgg',
-                    value = 'complex(0,1)*gY*gY/(16*pi2)',
+                    value = 'complex(0,1)*gY*gY/(16*9.86960440109)',
                     order = {'QCD': 2, 'QED': 0})
 
 UVGC_SYY = Coupling(name = 'UVGC_SYY',
-                    value = 'complex(0,1)*gY/(16*pi2)',
+                    value = 'complex(0,1)*gY/(16*9.86960440109)',
                     order = {'QCD': 1, 'QED': 1})
 
 UVGC_YYg = Coupling(name = 'UVGC_YYg',
-                    value = 'complex(0,1)*gS/(16*pi2)',
+                    value = 'complex(0,1)*gS/(16*9.86960440109)',
                     order = {'QCD': 2, 'QED': 0})
