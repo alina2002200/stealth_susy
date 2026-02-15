6db6ab79ffda58be2195dc92b44aad58d320404c
alina2002200 <71118359+alina2002200@users.noreply.github.com>
2026-02-15 22:22:27 +0300

    Update CT_vertices.py

--- Stealth_SUSY_NLO/CT_vertices.py
+++ Stealth_SUSY_NLO/CT_vertices.py
@@ -7,42 +7,66 @@ import particles as P
 import CT_couplings as C
 import lorentz as L
 
-V_2_Sgg_R2 = CTVertex(name = 'V_2_Sgg_R2',
-                      type = 'R2',
-                      particles = [ P.g, P.g, P.S__tilde__ ],
-                      color = [ 'Identity(1,2)' ],
-                      lorentz = [ L.VVS1 ],
-                      loop_particles = [ [ [P.Y] ] ],
-                      couplings = {(0,0,0):C.R2GC_Sgg})
+# R2 вершина для S -> gg (бесконечная часть)
+V_Sgg_R2 = CTVertex(name = 'V_Sgg_R2',
+                    type = 'R2',
+                    particles = [ P.g, P.g, P.S ],
+                    color = [ 'Identity(1,2)' ],
+                    lorentz = [ L.VVS1 ],
+                    loop_particles = [ [ [P.Y] ] ],
+                    couplings = {(0,0,0):C.GC_Sgg_r2})
 
-V_2_SYY_R2 = CTVertex(name = 'V_2_SYY_R2',
-                      type = 'R2',
-                      particles = [ P.Y__tilde__, P.Y, P.S__tilde__ ],
-                      color = [ 'Identity(1,2)' ],
-                      lorentz = [ L.FFS2 ],
-                      loop_particles = [ [ [P.g, P.Y] ] ],
-                      couplings = {(0,0,0):C.R2GC_SYY})
+# R2 вершина для S Y Y (нужна для полноты)
+V_SYY_R2 = CTVertex(name = 'V_SYY_R2',
+                    type = 'R2',
+                    particles = [ P.Y__tilde__, P.Y, P.S ],
+                    color = [ 'Identity(1,2)' ],
+                    lorentz = [ L.FFS2 ],
+                    loop_particles = [ [ [P.g, P.Y] ] ],
+                    couplings = {(0,0,0):C.GC_SYY_r2})
 
-V_2_Sgg_UV = CTVertex(name = 'V_2_Sgg_UV',
-                      type = 'R2',
-                      particles = [ P.Y__tilde__, P.Y ],
-                      color = [ 'Identity(1,2)' ],
-                      lorentz = [ L.FF1 ],
-                      loop_particles = [ [ [P.g, P.Y] ] ],
-                      couplings = {(0,0,0):C.UVGC_Sgg})
+# UV вершина для пропагатора Y
+V_YY_UV = CTVertex(name = 'V_YY_UV',
+                   type = 'UV',
+                   particles = [ P.Y__tilde__, P.Y ],
+                   color = [ 'Identity(1,2)' ],
+                   lorentz = [ L.FF1 ],
+                   loop_particles = [ [ [P.g, P.Y] ] ],
+                   couplings = {(0,0,0):C.GC_Sgg_ct})
 
-V_2_SYY_UV = CTVertex(name = 'V_2_SYY_UV',
-                      type = 'UV',
-                      particles = [P.Y__tilde__, P.Y, P.S__tilde__ ],
-                      color = [ 'Identity(1,2)' ],
-                      lorentz = [ L.FFS2 ],
-                      loop_particles = [ [ [P.g, P.Y] ] ],
-                      couplings = {(0,0,0):C.UVGC_SYY})
+# UV вершина для S Y Y (контрчлен)
+V_SYY_UV = CTVertex(name = 'V_SYY_UV',
+                    type = 'UV',
+                    particles = [ P.Y__tilde__, P.Y, P.S ],
+                    color = [ 'Identity(1,2)' ],
+                    lorentz = [ L.FFS2 ],
+                    loop_particles = [ [ [P.g, P.Y] ] ],
+                    couplings = {(0,0,0):C.GC_SYY_ct})
 
-V_2_YYg_UV = CTVertex(name = 'V_2_YYg_UV',
-                      type = 'R2',
-                      particles = [ P.Y__tilde__, P.Y, P.g ],
-                      color = [ 'T(3,2,1)' ],
-                      lorentz = [ L.FFV1, L.FFV3, L.FFV4 ],
-                      loop_particles = [ [ [P.g, P.Y] ] ],
-                      couplings = {(0,2,1):C.UVGC_YYg})
+# UV вершина для Y Y g (контрчлен)
+V_YYg_UV = CTVertex(name = 'V_YYg_UV',
+                    type = 'UV',
+                    particles = [ P.Y__tilde__, P.Y, P.g ],
+                    color = [ 'T(3,2,1)' ],
+                    lorentz = [ L.FFV1 ],
+                    loop_particles = [ [ [P.g, P.Y] ] ],
+                    couplings = {(0,0,0):C.GC_YgY})
+
+# UV вершина для S -> gg (контрчлен)
+V_Sgg_UV = CTVertex(name = 'V_Sgg_UV',
+                    type = 'UV',
+                    particles = [ P.g, P.g, P.S ],
+                    color = [ 'Identity(1,2)' ],
+                    lorentz = [ L.VVS1 ],
+                    loop_particles = [ [ [P.Y] ] ],
+                    couplings = {(0,0,0):C.GC_Sgg_ct})
+
+# Список всех CT-вершин для экспорта
+all_CTvertices = [
+    V_Sgg_R2,
+    V_SYY_R2,
+    V_YY_UV,
+    V_SYY_UV,
+    V_YYg_UV,
+    V_Sgg_UV
+]
