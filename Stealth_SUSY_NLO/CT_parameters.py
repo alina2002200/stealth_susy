c82886668241cf2094a046ba4396bffc10df060f
alina2002200 <71118359+alina2002200@users.noreply.github.com>
2026-02-15 21:15:28 +0300

    Create CT_parameters.py

---
+++ Stealth_SUSY_NLO/CT_parameters.py
@@ -0,0 +1,8 @@
+# This file was automatically created by FeynRules 2.4.16
+# Mathematica version: 9.0 for Mac OS X x86 (64-bit) (January 24, 2013)
+# Date: Fri 21 Aug 2015 09:21:22
+
+
+from object_library import all_CTparameters, CTParameter
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
