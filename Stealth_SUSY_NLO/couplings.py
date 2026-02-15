21fbedebef0edcd07e05a1e3bddf11f7bbc3ae8c
alina2002200 <71118359+alina2002200@users.noreply.github.com>
2026-02-15 17:06:40 +0300

    Update couplings.py

--- Stealth_SUSY_NLO/couplings.py
+++ Stealth_SUSY_NLO/couplings.py
@@ -1,13 +1,10 @@
 # This file was automatically created by FeynRules 1.7.221
 # Mathematica version: 9.0 for Mac OS X x86 (64-bit) (January 24, 2013)
 # Date: Mon 7 Oct 2013 12:36:22
-import math
 
 from object_library import all_couplings, Coupling
 
 from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
-
-pi = math.pi
 
 GC_Sgg_ct = Coupling(name = 'GC_Sgg_ct',
                      value = 'complex(0,1)*gY**2/(16*pi**2)',
