4695f347097e7230e69ceca3f382e69912a944d8
alina2002200 <71118359+alina2002200@users.noreply.github.com>
2026-02-15 17:01:40 +0300

    Update couplings.py

--- Stealth_SUSY_NLO/couplings.py
+++ Stealth_SUSY_NLO/couplings.py
@@ -2,18 +2,19 @@
 # Mathematica version: 9.0 for Mac OS X x86 (64-bit) (January 24, 2013)
 # Date: Mon 7 Oct 2013 12:36:22
 import math
-pi = math.pi
 
 from object_library import all_couplings, Coupling
 
 from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
 
+pi = math.pi
+
 GC_Sgg_ct = Coupling(name = 'GC_Sgg_ct',
-                     value = 'complex(0,1)*gY**2/(16*pi**2) * (1/eps)',
+                     value = 'complex(0,1)*gY**2/(16*pi**2)',
                      order = {'QCD': 2})
 
 GC_SYY_ct = Coupling(name = 'GC_SYY_ct',
-                     value = 'complex(0,1)*gY/(16*pi**2) * (1/eps)',
+                     value = 'complex(0,1)*gY/(16*pi**2)',
                      order = {'QCD': 1})
 
 GC_Sgg_r2 = Coupling(name = 'GC_Sgg_r2',
