6166a1a936e3f76582fbcb47b8bf300b6e3e0581
alina2002200 <71118359+alina2002200@users.noreply.github.com>
2026-02-15 16:48:15 +0300

    Update r2.py

--- Stealth_SUSY_NLO/r2.py
+++ Stealth_SUSY_NLO/r2.py
@@ -1,19 +1,18 @@
-from object_library import all_vertices_r2, VertexR2
+from object_library import all_vertices, Vertex
 import particles as P
 import couplings as C
 import lorentz as L
 
+# R2 член для S -> g g
+V_Sgg_r2 = Vertex(name = 'V_Sgg_r2',
+                  particles = [ P.S, P.g, P.g ],
+                  color = [ 'f(1,2,3)' ],
+                  lorentz = [ L.VVS1 ],
+                  couplings = {(0,0):C.GC_Sgg_r2})
 
-V_Sgg_r2 = VertexR2(name = 'V_Sgg_r2',
-                    particles = [ P.S, P.g, P.g ],
-                    color = [ 'f(1,2,3)' ],
-                    lorentz = [ L.VVS1 ],
-                    couplings = {(0,0):C.GC_Sgg_r2})
-
-V_SYY_r2 = VertexR2(name = 'V_SYY_r2',
-                    particles = [ P.S, P.Y, P.Y__tilde__ ],
-                    color = [ 'Identity(2,3)' ],
-                    lorentz = [ L.FFS1, L.FFS12 ],
-                    couplings = {(0,0):C.GC_SYY_r2, (0,1):C.GC_SYY_r2})
-
-all_vertices_r2 = [ V_Sgg_r2, V_SYY_r2 ]
+# R2 член для S -> Y Y~
+V_SYY_r2 = Vertex(name = 'V_SYY_r2',
+                  particles = [ P.S, P.Y, P.Y__tilde__ ],
+                  color = [ 'Identity(2,3)' ],
+                  lorentz = [ L.FFS1, L.FFS12 ],
+                  couplings = {(0,0):C.GC_SYY_r2, (0,1):C.GC_SYY_r2})
